1582e1c48923b3b1a7ffa61a78f86f67aa5eda44
Drew Thomas <drew@cipherstash.com>
2025-03-04 09:54:33 +1100

    Start example of search on encrypted column
    Not working yet (needs Encrypt config support in Protect.js)

--- apps/drizzle/src/select.ts
+++ apps/drizzle/src/select.ts
@@ -2,17 +2,41 @@ import 'dotenv/config'
 import { db } from './db'
 import { users } from './db/schema'
 import { protectClient } from './protect'
+import { bindIfParam, sql } from 'drizzle-orm';
+import type { BinaryOperator, SQL, SQLWrapper } from 'drizzle-orm';
 
-const sql = db
+// const csEq: BinaryOperator = (left: SQLWrapper, right: unknown): SQL => {
+// 	return sql`cs_unique_v1(${left}) = cs_unique_v1(${bindIfParam(right, left)})`;
+// };
+
+// const csGt: BinaryOperator = (left: SQLWrapper, right: unknown): SQL => {
+// 	return sql`cs_ore_64_8_v1(${left}) > cs_ore_64_8_v1(${bindIfParam(right, left)})`;
+// };
+
+// const csLt: BinaryOperator = (left: SQLWrapper, right: unknown): SQL => {
+// 	return sql`cs_ore_64_8_v1(${left}) < cs_ore_64_8_v1(${bindIfParam(right, left)})`;
+// };
+
+const csMatch: BinaryOperator = (left: SQLWrapper, right: unknown): SQL => {
+	return sql`cs_match_v1(${left}) @> cs_match_v1(${bindIfParam(right, left)})`;
+};
+
+const filterInput= await protectClient.encrypt("abc", {
+  column: 'email',
+  table: 'users',
+})
+
+const query = db
   .select({
     email: users.email_encrypted,
   })
   .from(users)
+  .where(csMatch(users.email_encrypted, filterInput))
 
-const sqlResult = sql.toSQL()
+const sqlResult = query.toSQL()
 console.log('[INFO] SQL statement:', sqlResult)
 
-const data = await sql.execute()
+const data = await query.execute()
 
 const emails = await Promise.all(
   data.map(
