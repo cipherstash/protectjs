932e06b0e60da7bc6e371c1a80bceecad372fc56
Drew Thomas <drew@cipherstash.com>
2025-03-04 10:51:27 +1100

    Working search example with equality on email

--- apps/drizzle/src/select.ts
+++ apps/drizzle/src/select.ts
@@ -4,10 +4,32 @@ import { users } from './db/schema'
 import { protectClient } from './protect'
 import { bindIfParam, sql } from 'drizzle-orm';
 import type { BinaryOperator, SQL, SQLWrapper } from 'drizzle-orm';
+import { parseArgs } from 'node:util'
 
-// const csEq: BinaryOperator = (left: SQLWrapper, right: unknown): SQL => {
-// 	return sql`cs_unique_v1(${left}) = cs_unique_v1(${bindIfParam(right, left)})`;
-// };
+const getEmail = () => {
+  const { values, positionals } = parseArgs({
+    args: process.argv,
+    options: {
+      email: {
+        type: 'string',
+      },
+    },
+    strict: true,
+    allowPositionals: true,
+  })
+
+  return values.email
+}
+
+const email = getEmail()
+
+if (!email) {
+  throw new Error('Email is required')
+}
+
+const csEq: BinaryOperator = (left: SQLWrapper, right: unknown): SQL => {
+	return sql`cs_unique_v1(${left}) = cs_unique_v1(${bindIfParam(right, left)})`;
+};
 
 // const csGt: BinaryOperator = (left: SQLWrapper, right: unknown): SQL => {
 // 	return sql`cs_ore_64_8_v1(${left}) > cs_ore_64_8_v1(${bindIfParam(right, left)})`;
@@ -17,21 +39,25 @@ import type { BinaryOperator, SQL, SQLWrapper } from 'drizzle-orm';
 // 	return sql`cs_ore_64_8_v1(${left}) < cs_ore_64_8_v1(${bindIfParam(right, left)})`;
 // };
 
-const csMatch: BinaryOperator = (left: SQLWrapper, right: unknown): SQL => {
-	return sql`cs_match_v1(${left}) @> cs_match_v1(${bindIfParam(right, left)})`;
-};
+// const csMatch: BinaryOperator = (left: SQLWrapper, right: unknown): SQL => {
+// 	return sql`cs_match_v1(${left}) @> cs_match_v1(${bindIfParam(right, left)})`;
+// };
 
-const filterInput= await protectClient.encrypt("abc", {
-  column: 'email',
+const filterInput = await protectClient.encrypt(email, {
+  column: 'email_encrypted',
   table: 'users',
 })
+
+if (filterInput.failure) {
+  throw new Error(`[protect]: ${filterInput.failure.message}`)
+}
 
 const query = db
   .select({
     email: users.email_encrypted,
   })
   .from(users)
-  .where(csMatch(users.email_encrypted, filterInput))
+  .where(csEq(users.email_encrypted, filterInput.data))
 
 const sqlResult = query.toSQL()
 console.log('[INFO] SQL statement:', sqlResult)
